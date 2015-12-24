216355410a536895d5732b3a7187072bf6d52d47
xupangen <xupangen@ffrj.net>
2015-12-24 19:19:28 +0800

    布局局部完成

--- myview/src/main/java/com/example/administrator/myview/seekbar/SeekBarActivity.java
+++ myview/src/main/java/com/example/administrator/myview/seekbar/SeekBarActivity.java
@@ -5,7 +5,9 @@ import com.example.administrator.myview.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 
@@ -15,6 +17,7 @@ import android.widget.SeekBar;
  * e-mail: xupangen@ffrj.net
  */
 public class SeekBarActivity extends Activity {
+    private static String TAG = "SeekBarActivity----->";
     private SeekBar seekbar = null;
     /**
      * 屏幕宽度
@@ -24,12 +27,13 @@ public class SeekBarActivity extends Activity {
      * 自定义随着拖动条一起移动的空间
      */
     private RelativeLayout textMoveLayout;
-
+    private LinearLayout hahha;
     /**
      * 托动条的移动步调
      */
     private float moveStep = 0;
     private ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +41,7 @@ public class SeekBarActivity extends Activity {
         setContentView(R.layout.a);
         screenWidth = getWindowManager().getDefaultDisplay().getWidth();
         textMoveLayout = (RelativeLayout) findViewById(R.id.textLayout);
+        hahha = (LinearLayout) findViewById(R.id.hahha);
         imageView = (ImageView) findViewById(R.id.testIV);
         /**
          * findView
@@ -51,13 +56,21 @@ public class SeekBarActivity extends Activity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        moveStep = (((float) (screenWidth - (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, getResources().getDisplayMetrics())
+        ) ));
+        moveStep = (float)((screenWidth-textMoveLayout.getWidth())/8);
+        Log.d(TAG,"screenWidth="+screenWidth);
+//        moveStep = (((float) (screenWidth / 9)));
+        Log.d(TAG + "moveStep", moveStep + "");
+    }
+
     public void searchVideos() {
         seekbar.setEnabled(true);
-        seekbar.setMax(9);
+        seekbar.setMax(8);
         Log.d("tag1", screenWidth + "");
-        moveStep = (float) (((screenWidth - 80 )/ 9));
-        Log.d("tag", moveStep + "");
-
     }
 
     private class OnSeekBarChangeListenerImp implements
@@ -66,12 +79,17 @@ public class SeekBarActivity extends Activity {
         // 触发操作，拖动
         public void onProgressChanged(SeekBar seekBar, int progress,
                                       boolean fromUser) {
-            Log.d("width", textMoveLayout.getWidth() + "");
-            Log.d("height", textMoveLayout.getHeight() + "");
-            Log.d("tagProgress",progress+"");
-            textMoveLayout.layout((int) (progress * moveStep), textMoveLayout.getTop(), textMoveLayout.getWidth() + (int) (progress * moveStep) , textMoveLayout.getBottom());
-            imageView.setAlpha((float) (progress % 9 * 0.1));
-            Log.d("aa", progress % 9 * 0.1 + "");
+
+            textMoveLayout.layout((int) (progress * moveStep), textMoveLayout.getTop(), textMoveLayout.getWidth() + (int) (progress * moveStep), textMoveLayout.getBottom());
+            imageView.setAlpha((float) (progress % 8 * 0.1));
+            Log.d(TAG + "aa", progress % 8 * 0.1 + "");
+            Log.d(TAG + "moveLayout-getWidth", textMoveLayout.getWidth() + "");
+            Log.d(TAG + "moveLayout-PaddingWidth", textMoveLayout.getLeft() + "");
+            Log.d(TAG + "seekBarParent", hahha.getWidth() + "");
+            Log.d(TAG + "seekBarWidth", seekBar.getWidth() + "");
+            Log.d(TAG + "moveLayout-width", textMoveLayout.getWidth() + "");
+            Log.d(TAG + "moveLayout-height", textMoveLayout.getHeight() + "");
+            Log.d(TAG + "tagProgress", progress + "");
 //            text.setText(getCheckTimeBySeconds(progress, startTimeStr));
         }
 
